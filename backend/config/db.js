32136f2ff22199a1bd3e51fc54bad8e509ce4eaa
Valentin <valentinrenaudin2206@gmail.com>
2024-12-06 14:53:22 +0100

    refactor: Update MongoDB connection URI and remove unused dependencies from package-lock.json

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('Connecté à MongoDB'))
+    .catch(err => console.error('Erreur de connexion MongoDB:', err));
     console.log('MongoDB connected');
   } catch (err) {
     console.error(err.message);
--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const io = new Server(server, {
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: '*', 
   methods: 'GET,POST,PUT,DELETE',
   credentials: true
 }));
